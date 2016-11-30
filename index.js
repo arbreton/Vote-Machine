78922752f23a50e9ebd65fc9116a6e2511f8960d
Alvaro Hernandez <alvaro.hernandez.010592@gmail.com>
2016-11-30 13:03:40 -0800

    Estructura de aplicacion

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var path = require('path');
+var cons = require('consolidate');
 
 // Conexión a base de datos
 mongoose.connect('mongodb://admin:root@ds153667.mlab.com:53667/mean-database');
@@ -11,22 +12,32 @@ mongoose.connect('mongodb://admin:root@ds153667.mlab.com:53667/mean-database');
 // Inicializar Express
 var app = express();
 
+
+// Require static assets from public folder
+app.engine('html', cons.swig)
+app.set('view engine', 'html');
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, '/public/views'));
+
+
+
+
 // Template engine
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // Configuracion
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + 'public'));
 
 // Rutas de las API
 
 
 // Inicio
-app.get('/', function (req, res) {
-	res.render('navbar');
+app.get('/votingapp', function (req, res) {
+	res.render('index', {page: 'index'});
 });
-
+/*
 // Rutas de las vistas
 app.get('/votacion', function (req, res) {
 	res.render('votacion');
@@ -34,7 +45,7 @@ app.get('/votacion', function (req, res) {
 
 app.get('/admin/candidatos', function (req, res) {
 	res.render('registrocandidatosview');
-});
+});*/
 
 
 
