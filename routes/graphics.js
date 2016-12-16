204e0f5737540ceae01e9e51bffb2f531bd4fcbc
vazquezmjr <vazquezmjr@gmail.com>
2016-12-16 09:59:40 -0800

    added extra graphics services

--- routes/graphics.js
+++ routes/graphics.js
@@ -33,20 +33,131 @@ app.post('/citizens', function(req, res) {
         });
     })
 //Get the citizens that voted in a certain election, chosen by date
-/*.get('/citizens/:fecha_eleccion',function(req, res) {
+.get('/citizens/:fecha_eleccion',function(req, res) {
         Citizen.find({'candidatos.presidenciales.fecha_eleccion':req.params.fecha_eleccion}, function(err, citizen) {
             if (err)
                 res.send(err);
             res.json(citizen);
         });
-    })*/
+    })
 //Get the citizens that voted in a certain election, chosen by date
-.get('/citizens/graph/:fecha_eleccion',function(req, res) {
-        Citizen.find({'candidatos.presidenciales.fecha_eleccion':req.params.fecha_eleccion}, function(err, citizen) {
+
+.get('/citizens/graph/gender',function(req, res) {
+       /* 
+        Citizen.where('edad').lte(19).select('clave_electoral nombre edad').exec(function(err, citizen) {
             if (err)
                 res.send(err);
             res.json(citizen);
         });
+*/
+
+        Citizen.aggregate(
+            [{ $match: {genero: "1"}},
+            { $group: { _id: "$provincia.distrito",Total_de_hombres:{ $sum: 1}}},
+            { $sort: {"Total_de_hombres":-1}}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+
+/*
+        Citizen.aggregate(
+            [{ $match: {genero: "1"}},
+            { $group: { _id: "$provincia.distrito",Total_de_hombres:{ $sum: 1}}},
+            { $sort: {"Total_de_hombres":-1}}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+*/
+
+/*
+       Citizen.count({'provincia.codigo':"101001",'genero'}, function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+        */
+    })
+
+.get('/citizens/graph/gender/f',function(req, res) {
+        Citizen.aggregate(
+            [{ $match: {genero: "2"}},
+            { $group: { _id: "$provincia.distrito",Total_de_mujeres:{ $sum: 1}}},
+            { $sort: {"Total_de_mujeres":-1}},
+            {$limit:5}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+
+
+    })
+.get('/citizens/graph/gender/both',function(req, res) {
+        Citizen.aggregate(
+            [
+            { $group: { _id: "$provincia.distrito",Total_de_mujeres:{ $sum: { $cond: [ { $eq: [ "$genero", "2"] } , 1, 0 ] }},
+            Total_de_hombres:{ $sum: { $cond: [ { $eq: [ "$genero", "1"] } , 1, 0 ] }}}},
+
+            { $sort: {"Total_de_mujeres":-1}},
+            {$limit:5}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+
+
+    })
+.get('/citizens/graph/gender/time',function(req, res) {
+        Citizen.aggregate(
+            [{ $match: {"candidatos.presidenciales.fecha_eleccion": "12-13-2016"}},
+            { $group: { _id: "$candidatos.presidenciales.estatus_votacion.hora_votacion",Total_de_mujeres:{ $sum: { $cond: [ { $eq: [ "$genero", "2"] } , 1, 0 ] }},
+            Total_de_hombres:{ $sum: { $cond: [ { $eq: [ "$genero", "1"] } , 1, 0 ] }}}},
+
+            { $sort: {"Total_de_mujeres":-1}},
+            {$limit:5}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+
+
+    })
+.get('/citizens/graph/gender/m',function(req, res) {
+        Citizen.aggregate(
+            [{ $match: {genero: "1"}},
+            { $group: { _id: "$provincia.distrito",Total_de_hombres:{ $sum: 1}}},
+            { $sort: {"Total_de_hombres":-1}},
+            {$limit:5}
+
+
+
+            ]).exec(function(err, citizen) {
+            if (err)
+                res.send(err);
+            res.json(citizen);
+        });
+
     })
 //Insert a vote into the database
 .put('/citizens/:citizen_id',function(req, res) {
@@ -70,6 +181,7 @@ app.post('/citizens', function(req, res) {
             presidencial.fecha_eleccion=req.body.fecha_eleccion;
             var estatusvotacion={};
             estatusvotacion.fecha_votacion=req.body.fecha_votacion;
+            estatusvotacion.hora_votacion=req.body.hora_votacion;
             estatusvotacion.voto=req.body.voto;
             presidencial.estatus_votacion=estatusvotacion;
             presidencial.otros=req.body.otros;
